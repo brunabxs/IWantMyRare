ce3930ed4bb3eed3308cf2cf91bb1c67623fb64b
Bruna Xavier <brunabxsantos@gmail.com>
2014-12-03 18:37:10 -0200

    Adicionando primeiros arquivos para o site

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('index');
+var PORT = (process.env.PORT || 5000);
+
+var restify = require('restify');
+var server = restify.createServer();
+
+server.use(restify.fullResponse());
+server.use(restify.bodyParser());
+
+server.get(/.*/, restify.serveStatic({
+  directory: './public'
+}));
+
+server.listen(PORT, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
