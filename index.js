88c6d34d7c0d4feaab9ba8802a49cb63dec93b25
Bruna Xavier <brunabxsantos@gmail.com>
2014-12-03 20:36:37 -0200

    Exibindo as informações dos raros
    - Servidor específico
    - Raros estão estáticos

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ var server = restify.createServer();
 server.use(restify.fullResponse());
 server.use(restify.bodyParser());
 
+server.get('/rares/:server', function (req, res, next) {
+  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+  res.end(JSON.stringify({server:req.params.server, 
+                          rares:[
+                            {
+                              name     :'Nakk',
+                              link     :'http://www.wowhead.com/npc=50990',
+                              location :{ region:'Nagrand', coords:[[77,44], [22,20]] },
+                              deaths   :[ '2014-03-12 00:00', '2014-03-12 00:00' ],
+                              respawn  :{ min:'10', max:'23' }
+                            },
+                            {
+                              name     :'Luk\'Hok',
+                              link     :'http://www.wowhead.com/npc=50981',
+                              location :{ region: 'Nagrand', coords: [[33,22], [11,5]] },
+                              deaths   :[ '2014-03-12 01:00', '2014-03-12 01:01' ],
+                              respawn  :{ min:'8', max:'12' }
+                            }
+                          ]}
+                        ));
+  return next();
+});
+
 server.get(/.*/, restify.serveStatic({
   directory: './public'
 }));
--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,16 @@
 jQuery(document).ready(function() {
+  jQuery.get('/rares/goldrinn')
+  .done(function(data) {
+    jQuery('tbody tr').remove();
+
+    jQuery.each(data.rares, function(index, rare) {
+      var row = jQuery('<tr></tr>');
+      row.append('<td>' + '<a class="edit"></a>' + '</td>');
+      row.append('<td><a href="' + rare.link + '">' + rare.name + '</a></td>');
+      row.append('<td>' + rare.deaths[0] + '</td>');
+      row.append('<td>' + rare.respawn.min + 'hrs ~ ' + rare.respawn.max + 'hrs</td>');
+      row.append('<td>' + rare.deaths[0] + ' ~ ' + rare.deaths[0] + '</td>');
+      jQuery('tbody').append(row);
+    });
+  });
 });
